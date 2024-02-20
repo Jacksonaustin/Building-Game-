cf8778d97538b132fb89fcf64a6e7254457c32cd
Jacksonaustin <111884165+Jacksonaustin@users.noreply.github.com>
2024-02-20 09:48:36 -0800

    Created main file

--- Main.py
+++ Main.py
@@ -1,1 +1,31 @@
-hello world
+import pygame 
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+Screen_Width = 1000
+Screen_Height = 1000
+
+screen = pygame.display.set_mode((Screen_Width, Screen_Height))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        
+        if(event.type == KEYDOWN):
+            if(event.key == K_ESCAPE):
+                running = False 
+
+        elif(event.type == QUIT):
+            running = False 
+            
